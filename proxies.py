c7ec6b011cb240de2515cf0e7db0567409721e41
Armen-Jean Andreasian <armen_andreasian@proton.me>
2025-04-24 11:44:43 +0700

    Create proxies.py

---
+++ proxies.py
@@ -0,0 +1,34 @@
+import json
+
+
+class ProxyGallery:
+    def __init__(self, proxy_file: str):
+        self._proxies: list[dict[str, str]] = self._load_proxies()
+        self.proxy_file = proxy_file
+
+    def _load_proxies(self):
+        with open(self.proxy_file, encoding='utf-8') as jf:
+            return json.load(jf)
+
+    @property
+    def generator(self):
+        for proxy in self._proxies:
+            yield proxy['IP'] + ':' + proxy['PORT']
+
+
+class ProxyProvider:
+    def __init__(self, proxy_file='proxies.json'):
+        proxy_gallery = ProxyGallery(proxy_file)
+        self._proxy_generator = proxy_gallery.generator
+        self._current_proxy = None
+
+    def _get_next_proxy(self):
+        try:
+            self._current_proxy = next(self._proxy_generator)
+        except StopIteration:
+            self._current_proxy = None
+
+    @property
+    def proxy(self):
+        self._get_next_proxy()
+        return self._current_proxy
